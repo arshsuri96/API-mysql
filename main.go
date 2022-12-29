993d779dfc0e122ca111ed3b627a5ccb74d4f56f
gunmala suri <gunmala@gunmalas-MacBook-Pro.local>
2022-12-29 13:29:48 +0530

    2nd commit

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -39,11 +41,47 @@ func main() {
 		dbName = "library"
 	}
 
+	l := library{
+		dbHost: dbHost,
+		dbPass: dbPass,
+		dbName: dbName,
+	}
 	r := mux.NewRouter()
-	r.HandleFunc(apiPath, getBooks).Methods("GET")
+	r.HandleFunc(apiPath, l.getBooks).Methods("GET")
 	http.ListenAndServe(":8080", r)
 }
 
-func getBooks(http.ResponseWriter, *http.Request) {
-	log.Printf("getBooks was called")
+func (l library) getBooks(http.ResponseWriter, *http.Request) {
+	//open connection
+	db := l.openConnection()
+	//read the books
+
+	books := []Book{}
+	rows, err := db.Query("select * from library")
+	if err != nil {
+		log.Fatalf("querying the book table %s\n", err.Error())
+	}
+	for rows.Next() {
+		var id, name, isbn string
+		err := rows.Scan(&id, &name, &isbn)
+		if err != nil {
+			log.Fatalf("error while scanning the row %s\n", err.Error())
+		}
+		aBook := Book{
+			Id:   id,
+			Name: name,
+			Isbn: isbn,
+		}
+		books = append(books, aBook)
+	}
+
+	//close connection
+}
+
+func (l library) openConnection() *sql.DB {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(%s)/%s", "root", l.dbHost, l.dbPass, l.dbName))
+	if err != nil {
+		log.Fatalf("opening the connection to DB %s\n", err.Error())
+	}
+	return db
 }
