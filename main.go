677623fb18c5930b49791e520d3c6f33e1f34698
gunmala suri <gunmala@gunmalas-MacBook-Pro.local>
2022-12-31 16:57:06 +0530

    added function get and post

--- main.go
+++ main.go
@@ -2,35 +2,40 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
-
-type library struct {
-	dbHost, dbPass, dbName string
-}
-
-type Book struct {
-	Id, Name, Isbn string
-}
 
 const (
 	API_PATH = "/apis/v1/books"
 )
 
+type Book struct {
+	Id, Name, Isbn string
+}
+
+type library struct {
+	dbHost, dbPass, dbName string
+}
+
 func main() {
+	// DB_HOST is of form host:port
 	dbHost := os.Getenv("DB_HOST")
 	if dbHost == "" {
 		dbHost = "localhost:3306"
 	}
+
 	dbPass := os.Getenv("DB_PASS")
 	if dbPass == "" {
 		dbPass = "arshsuri"
 	}
+
 	apiPath := os.Getenv("API_PATH")
 	if apiPath == "" {
 		apiPath = API_PATH
@@ -46,42 +51,77 @@ func main() {
 		dbPass: dbPass,
 		dbName: dbName,
 	}
+
 	r := mux.NewRouter()
-	r.HandleFunc(apiPath, l.getBooks).Methods("GET")
+	r.HandleFunc(apiPath, l.getBooks).Methods(http.MethodGet)
+	r.HandleFunc(apiPath, l.postBooks).Methods(http.MethodPost)
 	http.ListenAndServe(":8080", r)
 }
 
-func (l library) getBooks(http.ResponseWriter, *http.Request) {
+func (l library) postBooks(w http.ResponseWriter, r *http.Request) {
+	//read a request into an instance of book
+	book := Book{}
+	json.NewDecoder(r.Body).Decode(&book)
 	//open connection
 	db := l.openConnection()
-	//read the books
 
-	books := []Book{}
-	rows, err := db.Query("select * from library")
+	insertQuery, err := db.Prepare("insert into books values(?,?,?)")
 	if err != nil {
-		log.Fatalf("querying the book table %s\n", err.Error())
+		log.Fatalf("preparing the db query %s\n", err.Error())
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatalf("")
+	}
+	_, err = tx.Stmt(insertQuery).Exec(book.Id, book.Name, book.Isbn)
+	if err != nil {
+		log.Fatalf("executing the insert command %s\n", err.Error())
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatalf("Error while commiting the tx %s\n", err.Error())
+	}
+	//close the request
+	l.closeConnection(db)
+
+}
+
+func (l library) getBooks(w http.ResponseWriter, r *http.Request) {
+	books := []Book{}
+	db := l.openConnection()
+	rows, err := db.Query("select * from books")
+	if err != nil {
+		log.Fatalf("Querying from the table %s", err.Error())
 	}
 	for rows.Next() {
 		var id, name, isbn string
 		err := rows.Scan(&id, &name, &isbn)
 		if err != nil {
-			log.Fatalf("error while scanning the row %s\n", err.Error())
+			log.Fatalf("while scanning rows %s", err.Error())
 		}
-		aBook := Book{
+		abook := Book{
 			Id:   id,
 			Name: name,
 			Isbn: isbn,
 		}
-		books = append(books, aBook)
+		books = append(books, abook)
 	}
-
-	//close connection
+	json.NewEncoder(w).Encode(books)
+	l.closeConnection(db)
 }
 
 func (l library) openConnection() *sql.DB {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(%s)/%s", "root", l.dbHost, l.dbPass, l.dbName))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(%s)/%s", "root", l.dbPass, l.dbHost, l.dbName))
 	if err != nil {
-		log.Fatalf("opening the connection to DB %s\n", err.Error())
+		log.Fatalf("opening the connection to the database %s\n", err.Error())
 	}
 	return db
+}
+
+func (l library) closeConnection(db *sql.DB) {
+	err := db.Close()
+	if err != nil {
+		log.Fatalf("closing connection %s\n", err.Error())
+	}
 }
---
+++ scripts/sql.sh
@@ -0,0 +1,9 @@
+
+#creating database
+
+create database library;
+
+use library;
+create table books(id varchar(100), name varchar(100), isbn varchar(100));
+
+insert into books values ("1","Everything","ISBN-1");
